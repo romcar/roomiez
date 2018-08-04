79983d12380c40d3619fc47fb9231f8a2646e4a0
romcar <rommel.carrasquilla@gmail.com>
2018-08-04 17:26:38 -0400

    Create bare bones outline of where I want the componenets

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+import './App.css';
 
-const App = () => <div>Time to get serious</div>;
+const App = () => (
+	<div>
+		<Container>
+			<Row>
+				<Col className="outline">Nav Bar</Col>
+			</Row>
+			<Row>
+				<Col xl={3} md={3} sm={3} xs={false}>
+					{/* Side panel */}
+					<Container>
+						<Row className="outline">User Card</Row>
+						<Row className="outline">Notification System</Row>
+					</Container>
+				</Col>
+				{/* Main panel */}
+				<Col xl={8} md={8} sm={8} xs={false}>
+					<Container>
+						<Row className="outline">Room Listings</Row>
+						<Row className="outline">Random Quick Questions</Row>
+					</Container>
+				</Col>
+			</Row>
+		</Container>
+	</div>
+);
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				loader: 'css-loader'
+				loader: 'style-loader!css-loader'
 			}
 		]
 	}
