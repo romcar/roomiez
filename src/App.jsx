dbf711ff096bb1e98136735218cad0054f348a1d
romcar <rommel.carrasquilla@gmail.com>
2018-08-04 13:56:13 -0400

    Add eslint, rename index.js to .jsx, initialize webpack-dev-server

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+	// render() {
+	<div>Test</div>
+);
+// }
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
 const path = require('path'); // Meant for node have to use const
 
 module.exports = {
-  context: __dirname,
-  entry: "./index.js",
-  devtool: "cheap-eval-source-map",
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: true
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-    }]
-  }
-}
+	context: __dirname,
+	entry: './index.jsx', // usually the front door to the project.
+	devServer: {
+		publicPath: '/public/' // name of the path on the server
+	},
+	devtool: 'cheap-eval-source-map', // there are different ones that do different things
+	output: {
+		path: path.join(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.json'] // order of resolution L -> R
+	},
+	stats: {
+		// various things that get reported back to you when webpack is being built
+		colors: true,
+		reasons: true,
+		chunks: true
+	},
+	module: {
+		rules: [
+			{
+				enforce: 'pre',
+				test: /\.jsx?$/,
+				loader: 'eslint-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.jsx?$/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
