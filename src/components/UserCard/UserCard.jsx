d8d205e0e32e0d845025ad8b30c1da45f285746f
romcar <rommel.carrasquilla@gmail.com>
2018-08-06 13:54:50 -0400

    Add css file for userCard

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -6,14 +6,14 @@ const UserCard = () => (
 	<div>
 		<Container>
 			<Row>
-				<Col>
+				<Col xl={2.25} md={2.25} sm={2.25}>
 					<CardImg className="profile-pic" src="http://placekitten.com/120/120" />
 				</Col>
 				<Col>
-					<h4>user name</h4>
-					<div>chill level</div>
-					<div># qustions</div>
-					<div>isMoving button</div>
+					<h4 className="user-name">user name</h4>
+					<div className="chill-lvl">chill level</div>
+					<div className="questions-answered"># qustions</div>
+					<div className="isMoving">isMoving button</div>
 				</Col>
 			</Row>
 		</Container>
