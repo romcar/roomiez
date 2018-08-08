1bfbd47f01d2d258fd2e49cc7f999790d368969a
romcar <rommel.carrasquilla@gmail.com>
2018-08-08 14:47:00 -0400

    Make App container to a stateful component

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Col, Row } from 'reactstrap';
 import { Switch, Route } from 'react-router';
 import './App.css';
@@ -8,21 +8,24 @@ import Rooms from '../Rooms/Rooms';
 import NavBar from '../../components/NavBar/NavBar';
 import Messenger from '../Messenger/Messenger';
 
-const App = () => (
-	<div>
-		<Container>
-			<Row>
-				<Col className="outline">
-					<NavBar />
-				</Col>
-			</Row>
-		</Container>
-		<Switch>
-			<Route exact path="/" component={Home} />
-			<Route path="/rooms" component={Rooms} />
-			<Route path="/messenger" component={Messenger} />
-		</Switch>
-	</div>
-);
+class App extends Component {
+
+	render() {
+		return (<div>
+			<Container>
+				<Row>
+					<Col className="outline">
+						<NavBar />
+					</Col>
+				</Row>
+			</Container>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/rooms" component={Rooms} />
+				<Route path="/messenger" component={Messenger} />
+			</Switch>
+		</div>);
+	}
+};
 
 export default App;
