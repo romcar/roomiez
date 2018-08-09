f78211cb9dc4f707f13c4cbfcdaf1f98384ce55f
romcar <rommel.carrasquilla@gmail.com>
2018-08-09 13:12:36 -0400

    Add pagination functionality to RoomListings The RoomListings component can now update the number of listings that will be displayed depending on the size of the component. It also automatically updates the amount of pages.

--- src/components/RoomListings/RoomListing/RoomListing.jsx
+++ src/components/RoomListings/RoomListing/RoomListing.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './RoomListing.css';
 
-console.log(window.innerWidth);
 const RoomListing = ({roomListing}) => (
   <div className="roomListing">
     <img className="roomListingImage" src={roomListing.images[0]} alt="" />
--- src/components/RoomListings/RoomListings.jsx
+++ src/components/RoomListings/RoomListings.jsx
@@ -1,7 +1,7 @@
 /* eslint no-debugger:0 */
 import React, {Component} from 'react';
 import sizeMe from 'react-sizeme';
-import {Container, Col, Row} from 'reactstrap';
+import {Container, Row} from 'reactstrap';
 import './RoomListings.css';
 /*
 Components
@@ -13,19 +13,25 @@ class RoomListings extends Component {
     super(props);
     // console.log(props.size);
     this.state = {
+      currentPage: 1,
       roomListings: props.roomListings || [],
     };
     this.listingsPerPage = 6;
 
     this.setListingsPerPage = this.setListingsPerPage.bind(this);
     this.handleStateChange = this.handleStateChange.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+    this.prevPage = this.prevPage.bind(this);
+    this.renderListings = this.renderListings.bind(this);
+    this.calculateNumberOfPages = this.calculateNumberOfPages.bind(this);
   }
 
-  componentDidMount() {}
-
-  componentDidUpdate() {}
+  componentDidMount() {
+    this.setListingsPerPage();
+  }
 
   setListingsPerPage() {
+    const currentNumber = this.listingsPerPage;
     const {size} = this.props;
     if (size.width > 350 && size.width <= 500) {
       this.listingsPerPage = 2;
@@ -38,29 +44,67 @@ class RoomListings extends Component {
     } else if (size.width > 1000) {
       this.listingsPerPage = 6;
     }
+
+    if (currentNumber !== this.listingsPerPage) {
+      const {roomListings} = this.props;
+      this.calculateNumberOfPages(roomListings);
+    }
+  }
+
+  nextPage() {
+    const {currentPage, pageNumbers} = this.state;
+    const next =
+      currentPage === pageNumbers[pageNumbers.length - 1] ? 1 : currentPage + 1;
+    this.setState({currentPage: next});
+  }
+
+  prevPage() {
+    const {currentPage, pageNumbers} = this.state;
+    const prev =
+      currentPage === 1 ? pageNumbers[pageNumbers.length - 1] : currentPage - 1;
+    this.setState({currentPage: prev});
+  }
+
+  calculateNumberOfPages(listings) {
+    const numbers = [];
+    const j = Math.ceil(listings.length / this.listingsPerPage);
+    for (let i = 1; i <= j; i += 1) {
+      numbers.push(i);
+    }
+    this.setState({pageNumbers: numbers});
   }
 
   handleStateChange(name, value) {
     this.setState({[name]: value});
   }
 
+  renderListings(listings) {
+    this.x = 'idk';
+    return listings.map(roomListing => (
+      <RoomListing key={roomListing.id} roomListing={roomListing} />
+    ));
+  }
+
   render() {
-    const {roomListings} = this.state;
+    const {roomListings, currentPage} = this.state;
     this.setListingsPerPage();
+    const indexOfLastListing = this.listingsPerPage * currentPage;
+    const indexOfFirstListing = indexOfLastListing - this.listingsPerPage;
+    const listings = roomListings.slice(
+      indexOfFirstListing,
+      indexOfLastListing,
+    );
+    console.log(this.listingsPerPage);
     return (
-      <Container>
+      <Container className="roomListings">
         <Row>
-          <Col xl={1} lg={1} sm={1}>
-            <div className="roomListingsButton">{'<'}</div>
-          </Col>
-          <Col xl="auto" lg="auto" sm="auto">
-            {roomListings.map(roomListing => (
-              <RoomListing key={roomListing.id} roomListing={roomListing} />
-            ))}
-          </Col>
-          <Col xl={1} lg={1} sm={1}>
-            <div className="roomListingsButton">{'>'}</div>
-          </Col>
+          <div className="roomListingsButton" onClick={this.prevPage}>
+            {'<'}
+          </div>
+          <div className="tiles">{this.renderListings(listings)}</div>
+          <div className="roomListingsButton" onClick={this.nextPage}>
+            {'>'}
+          </div>
         </Row>
       </Container>
     );
